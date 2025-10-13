f1862649792a3467b4cd03f2c08c70fa342f3c4e
oluwaseyi4q <shoyinkaoluwaseyi531@gamil.com>
2025-10-13 21:06:14 +0100

    sixth commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 import { User } from '../models/User.js';
 import { generateNumericOtp } from '../utils/generateOtp.js';
-import { sendEmailOtp } from '../services/otpService.js';
+import { sendOtpEmail } from '../services/otpService.js';
 import { issueToken } from '../middlewares/auth.js';
 import {randomBytes}  from "crypto";
 
